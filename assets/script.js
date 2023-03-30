a0953fd4410ce3084b73d372e236b5622cf79b5d
Joseph Baek <josephbaek97@gmail.com>
2023-03-30 12:40:52 -0700

    Fixed Local Storage and Page reloads

--- assets/script.js
+++ assets/script.js
@@ -13,10 +13,12 @@ var weatherReport = document.querySelector(".weatherReport");
 console.log(weatherReport);
 var mainWeatherCard = document.querySelector(".mainWeatherCard")
 var pastSearches = document.querySelector('.pastSearches');
+var weatherCards = $(".weatherCard");
 var searchHistory = [];
 
 // Getting Coordinates
 function getCoordinates(search) {
+    $('li').remove();
     var q = search;
     var apiCallCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${apiKey}`;
     fetch(apiCallCoordinates) 
@@ -35,98 +37,185 @@ function getCoordinates(search) {
             localStorage.setItem('coordinates', JSON.stringify(data));
 
         })
+        .then(function getWeather(coordinates) {
+            var coordinate = JSON.parse(localStorage.getItem('coordinates'));
+            console.log(coordinate);
+            var lat = coordinate[0].lat;
+            console.log(lat);
+            var lon = coordinate[0].lon;
+            console.log(lon);
+            var apiCallWeather = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+            console.log(apiCallWeather);
+            fetch(apiCallWeather) 
+                .then(function(response) {
+                    if (!response.ok) {
+                        throw new Error('Could not use the latitude and longitude in order to find weather for that city');
+                    }
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    localStorage.setItem('weather', JSON.stringify(data));
+                })
+                .then(function addCurrentWeather(weather) {
+                    $('fig').remove();
+                    var weatherData = JSON.parse(localStorage.getItem('weather'));
+                    console.log(weatherData);
+                    var name = weatherData.city.name;
+                    console.log(name);
+                    var date = weatherData.list[0].dt_txt;
+                    console.log(date);
+                    var sky = weatherData.list[0].weather[0].icon;
+                    console.log(sky);
+                    var temp = weatherData.list[0].main.temp;
+                    // Convert to faranheit
+                    var tempFaranheit = `${temp} °F`
+                    console.log(tempFaranheit);
+                    var wind = weatherData.list[0].wind.speed;
+                    var windSpeed = `${wind} mph`;
+                    console.log(windSpeed);
+                    var humidity = weatherData.list[0].main.humidity;
+                    var humidityPercentage = `${humidity} %`;
+                    console.log(humidityPercentage);
+                
+                    var weatherObject = {
+                        Name: name,
+                        Date: date,
+                        Icon: sky,
+                        Temperature: tempFaranheit,
+                        Wind: windSpeed,
+                        Humidity: humidityPercentage
+                    };
+                    console.log(weatherObject);
+                    mainWeatherCard.innerHTML += `<fig class="currentWeatherCard"><h2>${weatherObject.Name}</h2><h3>${weatherObject.Date}</h3><img src="http://openweathermap.org/img/wn/${weatherObject.Icon}@2x.png"><h3>${weatherObject.Temperature}</h3><h3>${weatherObject.Wind}</h3><h3>${weatherObject.Humidity}</h3></fig>`;
+                    localStorage.setItem(`Day 1`, JSON.stringify(weatherObject));}
+                )
+                .then(function add5DayWeather() {
+                    var weatherData = JSON.parse(localStorage.getItem('weather'));
+                    console.log(weatherData);
+                    for(i = 1; i < 6; i++) {
+                    var date = weatherData.list[i].dt_txt;
+                    console.log(date);
+                    var sky = weatherData.list[i].weather[0].icon;
+                    console.log(sky);
+                    var temp = weatherData.list[i].main.temp;
+                    // Convert to faranheit
+                    var tempFaranheit = `${temp} °F`
+                    console.log(tempFaranheit);
+                    var wind = weatherData.list[i].wind.speed;
+                    var windSpeed = `${wind} mph`;
+                    console.log(windSpeed);
+                    var humidity = weatherData.list[i].main.humidity;
+                    var humidityPercentage = `${humidity} %`;
+                    console.log(humidityPercentage);
+                
+                    var weatherObject = {
+                        Date: date,
+                        Icon: sky,
+                        Temperature: tempFaranheit,
+                        Wind: windSpeed,
+                        Humidity: humidityPercentage
+                    };
+                    console.log(weatherObject);
+                    weatherReport.innerHTML += `<li class="weatherCard"><h3>${weatherObject.Date}</h3><img src="http://openweathermap.org/img/wn/${weatherObject.Icon}@2x.png"><h3>${weatherObject.Temperature}</h3><h3>${weatherObject.Wind}</h3><h3>${weatherObject.Humidity}</h3></li>`;
+                    localStorage.setItem(`Day ${i + 1}`, JSON.stringify(weatherObject));
+                }
+                
+                }
+                )
+        })
 };
 
 // Getting Weather 
-function getWeather (coordinate) {
-    var coordinates = JSON.parse(localStorage.getItem('coordinates'));
-    console.log(coordinates);
-    var lat = coordinates[0].lat;
-    console.log(lat);
-    var lon = coordinates[0].lon;
-    console.log(lon);
-    var apiCallWeather = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    console.log(apiCallWeather);
-    fetch(apiCallWeather) 
-        .then(function(response) {
-            if (!response.ok) {
-                throw new Error('Could not use the latitude and longitude in order to find weather for that city');
-            }
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            localStorage.setItem('weather', JSON.stringify(data));
-        })
-};
+// function getWeather(coordinates) {
+//     var coordinate = JSON.parse(localStorage.getItem('coordinates'));
+//     console.log(coordinate);
+//     var lat = coordinate[0].lat;
+//     console.log(lat);
+//     var lon = coordinate[0].lon;
+//     console.log(lon);
+//     var apiCallWeather = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+//     console.log(apiCallWeather);
+//     fetch(apiCallWeather) 
+//         .then(function(response) {
+//             if (!response.ok) {
+//                 throw new Error('Could not use the latitude and longitude in order to find weather for that city');
+//             }
+//             return response.json();
+//         })
+//         .then(function(data) {
+//             console.log(data);
+//             localStorage.setItem('weather', JSON.stringify(data));
+//         })
+// };
 
 // Adding Current Weather Card
-function addCurrentWeather() {
-    var weatherData = JSON.parse(localStorage.getItem('weather'));
-    console.log(weatherData);
-    var name = weatherData.city.name;
-    console.log(name);
-    var date = weatherData.list[0].dt_txt;
-    console.log(date);
-    var sky = weatherData.list[0].weather[0].icon;
-    console.log(sky);
-    var temp = weatherData.list[0].main.temp;
-    // Convert to faranheit
-    var tempFaranheit = `${temp} °F`
-    console.log(tempFaranheit);
-    var wind = weatherData.list[0].wind.speed;
-    var windSpeed = `${wind} mph`;
-    console.log(windSpeed);
-    var humidity = weatherData.list[0].main.humidity;
-    var humidityPercentage = `${humidity} %`;
-    console.log(humidityPercentage);
+// function addCurrentWeather() {
+//     var weatherData = JSON.parse(localStorage.getItem('weather'));
+//     console.log(weatherData);
+//     var name = weatherData.city.name;
+//     console.log(name);
+//     var date = weatherData.list[0].dt_txt;
+//     console.log(date);
+//     var sky = weatherData.list[0].weather[0].icon;
+//     console.log(sky);
+//     var temp = weatherData.list[0].main.temp;
+//     // Convert to faranheit
+//     var tempFaranheit = `${temp} °F`
+//     console.log(tempFaranheit);
+//     var wind = weatherData.list[0].wind.speed;
+//     var windSpeed = `${wind} mph`;
+//     console.log(windSpeed);
+//     var humidity = weatherData.list[0].main.humidity;
+//     var humidityPercentage = `${humidity} %`;
+//     console.log(humidityPercentage);
 
-    var weatherObject = {
-        Name: name,
-        Date: date,
-        Icon: sky,
-        Temperature: tempFaranheit,
-        Wind: windSpeed,
-        Humidity: humidityPercentage
-    };
-    console.log(weatherObject);
-    mainWeatherCard.innerHTML += `<fig class="currentWeatherCard"><h2>${weatherObject.Name}</h2><h3>${weatherObject.Date}</h3><img src="http://openweathermap.org/img/wn/${weatherObject.Icon}@2x.png"><h3>${weatherObject.Temperature}</h3><h3>${weatherObject.Wind}</h3><h3>${weatherObject.Humidity}</h3></fig>`;
-    localStorage.setItem(`Day 1`, JSON.stringify(weatherObject));
-}
+//     var weatherObject = {
+//         Name: name,
+//         Date: date,
+//         Icon: sky,
+//         Temperature: tempFaranheit,
+//         Wind: windSpeed,
+//         Humidity: humidityPercentage
+//     };
+//     console.log(weatherObject);
+//     mainWeatherCard.innerHTML += `<fig class="currentWeatherCard"><h2>${weatherObject.Name}</h2><h3>${weatherObject.Date}</h3><img src="http://openweathermap.org/img/wn/${weatherObject.Icon}@2x.png"><h3>${weatherObject.Temperature}</h3><h3>${weatherObject.Wind}</h3><h3>${weatherObject.Humidity}</h3></fig>`;
+//     localStorage.setItem(`Day 1`, JSON.stringify(weatherObject));
+// }
 
 // Adding 5 Day Weather Cards
-function add5DayWeather() {
-    var weatherData = JSON.parse(localStorage.getItem('weather'));
-    console.log(weatherData);
-    for(i = 1; i < 6; i++) {
-    var date = weatherData.list[i].dt_txt;
-    console.log(date);
-    var sky = weatherData.list[i].weather[0].icon;
-    console.log(sky);
-    var temp = weatherData.list[i].main.temp;
-    // Convert to faranheit
-    var tempFaranheit = `${temp} °F`
-    console.log(tempFaranheit);
-    var wind = weatherData.list[i].wind.speed;
-    var windSpeed = `${wind} mph`;
-    console.log(windSpeed);
-    var humidity = weatherData.list[i].main.humidity;
-    var humidityPercentage = `${humidity} %`;
-    console.log(humidityPercentage);
+// function add5DayWeather() {
+//     var weatherData = JSON.parse(localStorage.getItem('weather'));
+//     console.log(weatherData);
+//     for(i = 1; i < 6; i++) {
+//     var date = weatherData.list[i].dt_txt;
+//     console.log(date);
+//     var sky = weatherData.list[i].weather[0].icon;
+//     console.log(sky);
+//     var temp = weatherData.list[i].main.temp;
+//     // Convert to faranheit
+//     var tempFaranheit = `${temp} °F`
+//     console.log(tempFaranheit);
+//     var wind = weatherData.list[i].wind.speed;
+//     var windSpeed = `${wind} mph`;
+//     console.log(windSpeed);
+//     var humidity = weatherData.list[i].main.humidity;
+//     var humidityPercentage = `${humidity} %`;
+//     console.log(humidityPercentage);
 
-    var weatherObject = {
-        Date: date,
-        Icon: sky,
-        Temperature: tempFaranheit,
-        Wind: windSpeed,
-        Humidity: humidityPercentage
-    };
-    console.log(weatherObject);
-    weatherReport.innerHTML += `<li class="weatherCard"><h3>${weatherObject.Date}</h3><img src="http://openweathermap.org/img/wn/${weatherObject.Icon}@2x.png"><h3>${weatherObject.Temperature}</h3><h3>${weatherObject.Wind}</h3><h3>${weatherObject.Humidity}</h3></li>`;
-    localStorage.setItem(`Day ${i + 1}`, JSON.stringify(weatherObject));
-}
+//     var weatherObject = {
+//         Date: date,
+//         Icon: sky,
+//         Temperature: tempFaranheit,
+//         Wind: windSpeed,
+//         Humidity: humidityPercentage
+//     };
+//     console.log(weatherObject);
+//     weatherReport.innerHTML += `<li class="weatherCard"><h3>${weatherObject.Date}</h3><img src="http://openweathermap.org/img/wn/${weatherObject.Icon}@2x.png"><h3>${weatherObject.Temperature}</h3><h3>${weatherObject.Wind}</h3><h3>${weatherObject.Humidity}</h3></li>`;
+//     localStorage.setItem(`Day ${i + 1}`, JSON.stringify(weatherObject));
+// }
 
-};
+// };
 
 // Getting Search 
 function getSearch(event) {
@@ -135,9 +224,10 @@ function getSearch(event) {
     searchHistory.push(searchResult);
     console.log(searchResult);
     getCoordinates(searchResult);
-    getWeather();
-    addCurrentWeather();
-    add5DayWeather();
+    console.log(latitude);
+    // getWeather(coordinates);
+    // addCurrentWeather();
+    // add5DayWeather();
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     var history = JSON.parse(localStorage.getItem('searchHistory'));
     console.log(history);
